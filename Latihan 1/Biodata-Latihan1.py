9a6a209fced5024fa2b88b5551bfdb6cb7c19686
ryanalinputra <ryanalinputraa@gmail.com>
2025-09-06 15:32:13 +0700

    first commit

---
+++ Latihan 1/Biodata-Latihan1.py
@@ -0,0 +1,13 @@
+na = "Ryan Alin Putra"
+ni = "12345678"
+ps = "D3-Manajemen Informatika"
+ke = "1A"
+al = "Dsn.Sumbersari RT03/RW06 Ds.Datengan Kec.Grogol Kab.Kediri"
+
+print (
+    "Nama   : " + na + "\n" +
+    "Nim    : " + ni + "\n" +
+    "Kelas  : " + ke + "\n" +
+    "Prodi  : " + ps + "\n" +
+    "Alamat : " + al
+)
